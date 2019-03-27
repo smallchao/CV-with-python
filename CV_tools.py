6e724d3bf032d0766bbb92f5294186d5fc766a5f
superchao <411303959@qq.com>
2019-03-27 17:08:38 +0800

    Update CV_tools.py
    fix a bug

--- CV_tools.py
+++ CV_tools.py
@@ -53,16 +53,16 @@ def show_color(im):
     '''
     # im = mpimg.imread('wa_state_highway.jpg')
     r = im[:,:,0]
-	g = im[:,:,1]
-	b = im[:,:,2]
+    g = im[:,:,1]
+    b = im[:,:,2]
 
-	f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(20,10))
-	ax1.set_title('R channel')
-	ax1.imshow(r, cmap='gray')
-	ax2.set_title('G channel')
-	ax2.imshow(g, cmap='gray')
-	ax3.set_title('B channel')
-	ax3.imshow(b, cmap='gray')
+    f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(20,10))
+    ax1.set_title('R channel')
+    ax1.imshow(r, cmap='gray')
+    ax2.set_title('G channel')
+    ax2.imshow(g, cmap='gray')
+    ax3.set_title('B channel')
+    ax3.imshow(b, cmap='gray')
 
 def image_process_batch(path):
     '''
